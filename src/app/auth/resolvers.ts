ef0f58411d09e6935d11b6edf7a5232013e8b590
shubhswain45 <shubhkantswain@.com>
2024-11-08 07:53:32 +0530

    some fixes

--- src/app/auth/resolvers.ts
+++ src/app/auth/resolvers.ts
@@ -77,7 +77,7 @@ const mutations = {
 
             ctx.res.cookie("__Moments_token", userToken, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
+                secure: false,
                 sameSite: 'strict',
                 maxAge: 24 * 60 * 60 * 1000 // 1 day
             });
